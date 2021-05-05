79ef54cae9f47a645bd40ce434fcff5282d648a4
sameer <sameer.barha12@gmail.com>
2021-05-05 13:11:42 +0530

    added module to handle formatting messages

---
+++ messages/messages.py
@@ -0,0 +1,45 @@
+'''
+formatting messages
+'''
+
+def prepare_center_name(center_idx, center):
+
+    #prepare center banner
+    center_name = '''\
+===============================
+{count}) {center_name}
+{block_name}
+{district_name}
+{state_name}
+{pincode}
+Timing :{time_from} - {to}
+Fee : {fee_type}
+--------------------------------
+\
+'''.format(count=center_idx,center_name=center["name"], block_name=center["block_name"],district_name=center["district_name"], state_name=center["state_name"], pincode=center["pincode"],time_from=center['from'], to=center['to'], fee_type=center["fee_type"])
+
+    return center_name
+
+def prepare_center_sessions(session):
+
+    #prepare session data
+    session_name ='''\
+Date: {date}
+Capacity Avail.: {available_capacity}
+Vaccine: {vaccine}
+Min. Age: {min_age}
+\n\
+'''.format(date=session["date"], available_capacity=session["available_capacity"], vaccine=session["vaccine"], min_age=session["min_age_limit"])
+
+    return session_name
+
+def prepare_center_price(vaccine, fee):
+
+    #prepare vaccination  price
+    text_body = '''\
+Vaccines Avail:
+{vaccine} : ₹{price}
+\
+'''.format(vaccine=vaccine, price=fee)
+
+    return text_body
